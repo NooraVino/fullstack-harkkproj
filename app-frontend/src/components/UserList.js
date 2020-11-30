61b875d4781549ce27c887b1e6fa41b2f34eaf80
NooraVino <noora.virolainen@yahoo.com>
2020-11-30 13:22:13 +0200

    new gitirnore

--- app-frontend/src/components/UserList.js
+++ app-frontend/src/components/UserList.js
@@ -1,11 +1,9 @@
 import React, {useState, useEffect  } from 'react';
 import axios from 'axios'
 
-
 const UserList = () => {
   const [users, setUsers] = useState([]);
 
-  
   useEffect(() => {
   axios
   .get('/api/users')
